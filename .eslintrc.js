66c44760c0a65fdd55781909006b8ad86eef82d3
Elf-mousE <ifmouz@gmail.com>
2019-03-11 17:01:05 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  env: {
+    node: true
+  },
+  extends: 'eslint-config-egg'
+};
---
+++ config/config.default.js
@@ -0,0 +1,30 @@
+/* eslint valid-jsdoc: "off" */
+
+const SECURITY_COOKIE_KEYS = '_1552258990018_5464';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + SECURITY_COOKIE_KEYS;
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  return {
+    ...config,
+    ...userConfig
+  };
+};
