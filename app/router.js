74e52e8fae27a34bc6f7447d60575bb2a628e966
Elf-mousE <ifmouz@gmail.com>
2019-03-12 16:40:10 +0800

    update

--- app/router.js
+++ app/router.js
@@ -5,4 +5,5 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   router.get('/news', controller.news.list);
+  router.get('/user/info/:id', controller.user.info);
 };
--- config/plugin.js
+++ config/plugin.js
@@ -7,5 +7,9 @@ module.exports = {
   nunjucks: {
     enable: true,
     package: 'egg-view-nunjucks'
+  },
+  mysql: {
+    enable: true,
+    package: 'egg-mysql'
   }
 };
